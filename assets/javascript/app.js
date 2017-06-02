4d57bac33cbbb569d7ac78e879a8bcc19c9ebd6e
HayelomR <rhayelom@gmail.com>
2017-06-02 09:39:48 -0600

    more fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,107 +1,262 @@
-//pseudo codehe
-//the first screen should show start the game.
-//on each questions there should be timer.
-//let a player chooose an answer form the choice words
-//if not answer correct show the correct answer and move to the next
-// let final screen show which ones are the correct and wrong ones with the rsult
-//variables
 
-var correctAnswer = 0;//number of questions that will be answered
-var wrongAnswer = 0;//number of questions that are answerd wrong
-var unAnswered = 0;//number of questions unanswerd b/c time is up
-var firstScreen;
-var counter = 0;//number of questions will be counted here
-var clock = "";//time tracker
-//var firstScreen = ;// the first screen that will be shown first with the button
+//var trivTime = 0; this will be the amount of time allowed for each question
+var rightAnswer = 0;//this counts the number of right answers.
+var wrongAnswer = 0;//this counts the number of wrong answers
+var questionCounter = 1;// this counts the number of questions.
+//var timer = ''; this will be the timer that will be shown for each question.
+// lets create an object so that we can access questions and answers pull our questions.
+var questions = {
+			1:{
+				question:"What ocean is on the West Coast of the United States?",
+				answers:["Atlantic","Southern","Arctic","Pacific"],
+				correct: "Pacific",
+				right: "Correct!",
+				wrong: "Wrong!",
 
+			   },
 
-var questions ={
+			2:{
+				question:"What do we show loyalty to when we say the Pledge of Allegiance?",
+				answers:["The President","USA","State","Congress"],
+				correct:"USA",
+				right: "Correct!",
+				wrong: "Wrong!",
+	
+			},
+			3:{
+				question:"Before he was President, Eisenhower was a general. What war was he in?",
+				answers:["Vietnam War","Civil War","World War II","Spanish-American War"],
+				correct:"World War II",
+				right: "Correct!",
+				wrong: "Wrong!",
+		
+			},
 
-	q1:{
-		question: "how many amendments does the constitution have?",
-		choices: ["20","14","27","8"],
-		answer: "27",
-	},
+			4:{
+				question:"We elect a President for how many years?",
+				answers:["Four","Eight","Six","Two"],
+				correct:"Four",
+				right: "Correct!",
+				wrong: "Wrong!",
+			},
+			
+			5:{
+				question:"Name one problem that led to the Civil War.",
+				answers:["Sugar","Slavery","West ward expansion","Oil","none of the above"],
+				correct:"none of the above",
+				right: "Correct!",
+				wrong: "Wrong!",
+			},
 
-	q2: {
-		question:"We elect a U.S. Representative for how many years?",
-		choices: ["2","6","4","8"],
-		answer: "2",
-
-	},
-
-	q3:{
-		question:"How many U.S. Senators are there?",
-		choices:["50","40","100","121"],
-		answer:"100",
-	},
-
-	q4:{
-		question:"We elect a U.S. Senator for how many years?",
-		choices:["2","8","6","10"],
-		answer:"6",
-	},
-
-	q5:{ 
-		question:"The House of Representatives has how many voting members?",
-		choices:["100","235","435","935"],
-		answer:"435",
-	}
-};
-
-//lets ceate an array questions from the above object
-var createdQuestions = [questions.q1,questions.q2,questions.q3,questions.q4,questions.q5];
-
- // before we start the game lets empty the first screen
-function stratGame(){
-	$(".startButton").on("click",function(){
- 		$(".firstScreen").empty();
- 		createdQuestions();
-
+	};
+//lets start the game //When buttons is clicked the first screen will be cleared and the second will come
+function start(){
+	$(".startBtn").on("click",function(){
+		
+		$(".gamePage").html("");// clears the second screen and make it ready
+		// this is the same as $(".gamePage").empty();
+		createQuestions();
 	});
 }
 
-//lests create answers 
-function answerForQuestions(){
-	var answerLength = questions[questionsCounter]["answers"].length;
+ function createQuestions(){
+	//timerStart();
+	//Get question
+	var question = questions[questionCounter]["question"];
+	//assign div element to newDiv
+	var newDiv = $("<div>");
+	//Add a class to newDIv
+	newDiv.append("question");
+	//Add text to question
+	newDiv.text(question);
+	//Add question to DOM
+	$(".gamePage").append(newDiv);
+	createAnswers();
+}
+ function createAnswers(){
+	var answerLength = questions[questionCounter]["answers"].length;
 	for(var i = 0; i < answerLength;i++){
 		//get answers
-		var answers = questins[counter]["answers"][i];
+		var answers = questions[questionCounter]["answers"][i];
 		//Create new div to hold answers
 		var newBtn = $("<button>");
-		//Add class to new Div
-		newBtn.addClass("answers redBtn");
+				//Add class to new Div"
+		newBtn.addClass("answers");
 		//Give buttons attribute
 		newBtn.attr("data-type",answers);
 		//add text to new Div
 		newBtn.text(answers);
 		//Add answers to DOM
-		$(".firstScreen").append(newBtn);
+		$(".gamePage").append(newBtn);
 	}
-	
+	//Prevents click event from being saved
+	$(document).off("click",".answers",checkAnswer);
+	$(document).on("click",".answers",checkAnswer);
 }
- 
+ function checkAnswer(){
+	 //Get users answer choice
+	var userAnswer = $(this).data("type");
+	var correctAnswer = questions[questionCounter]["correct"];
+	var right = questions[questionCounter]["right"];
+	var wrong = questions[questionCounter]["wrong"];
+	//console.log(qACount);
+	if(userAnswer === correctAnswer){
+		//Update rightCount
+		rightAnswer++;
+		//Clears out triv Section
+		$(".gamePage").empty();
 
+		var newDiv = $("<div>");
+		//Give div class
+		newDiv.addClass("correctAnswer");
+		//adds CORRECT! text to div
+		newDiv.text(right);
+		//Add answer to DOM
+		$(".gamePage").append(newDiv);
+		//Stops Time
+		//clearInterval(timer)
+		//Add 1 to question count to move to the next question
+		questionCounter++;
+		if(questionCounter <= 5){
+			//removes CORRECT! text and continues to create next question after 3 seconds
+			setTimeout(
+				function(){
+					$(".gamePage").empty();
+					createQuestions();
+					},500); //1500 milliseconds the time it take to go from one question to another
+		}
+		else{
+			$(".gamePage").empty();
+			// var newImg = $('<img>');
+			// newImg.attr('src',correctImg);
+			// $('.trivSection').append(newImg);
+			//Create Div
+			var newDiv = $("<div>");
+			//Give div class
+			newDiv.addClass("correctAnswer");
+			//adds CORRECT! text to div
+			newDiv.text(right);
+			//Add answer to DOM
+			$(".gamePage").append(newDiv);
+			//Stops Time
+			//clearInterval(timer)
+			//Reset
+			setTimeout(gameOver, 500);
+		}
+	}
+	else{
+		wrongAnswer++;
+		//Clears out triv Section
+		$(".gamePage").empty();
+		// var newImg = $('<img>');
+		// newImg.attr('src',correctImg);
+		// $('.trivSection').append(newImg);
+		var newDiv = $("<div>");
+		//Give div class
+		newDiv.addClass("incorrectAnswer");
+		//adds Wrong! text to div
+		newDiv.text(wrong);
+		//Add answer to DOM
+		$(".gamePage").append(newDiv);
+		//Stops Time
+		//clearInterval(timer)
+		//Add 1 to question count to move to the next question
+		questionCounter++;
+		
+		if(questionCounter <= 5){
+			setTimeout(function(){
+			$(".gamePage").empty();
+			createQuestions();
+			},500);
+		}
+		else{
+			//Clears out triv Section
+			$(".gamePage").empty();
+		// 	var newImg = $('<img>');
+		// newImg.attr('src',correctImg);
+		// $('.trivSection').append(newImg);
+			var newDiv = $("<div>");
+			//Give div class
+			newDiv.addClass("incorrectAnswer");
+			//adds Wrong! text to div
+			newDiv.text(wrong);
+			//Add answer to DOM
+			$(".gamePage").append(newDiv);
+			//Stops Time
+			//clearInterval(timer);
+			//Reset
+			setTimeout(gameOver, 500);
+		}
+	}
+}
+// //Timer
+// //==========================================
+// var timerStart = function(){ 
+// 	$('.timerSection').empty();
+// 	//Sets time to 10
+// 	trivTime = 100;
+// 	//Progress Bar
+// 	var timeTag = $('<div>');
+// 	timeTag.addClass('time');
+// 	timeTag.addClass('progress');
+// 	var progressBar = $('<div>');
+// 	progressBar.addClass('progress-bar');
+// 	progressBar.width(trivTime + '%');
 
-// lets create a list of the answer choice as mulitple choices.
-
-
-//lets create an array of question from the object above
-//var questionA = [questions.1, questions.2, questions.3,questions.4,questins.5];
-//console.log(question.1);
-// lets create radio for the multiple questions.
-
-// function 
-// function numberOfWins (){
-// 	correctAnswer++;
+// 	$('.timerSection').append(timeTag);
+// 	$('.time').append(progressBar);	
+// 	//Decrements Time
+// 	timer = setInterval(timeDecrement,100);
 // }
-// $(document).ready(function(){
-
-// function initalScreen (){
-// $(".button").text("firstScreen");
-// alert("working");
+// var timeDecrement = function(){ 
+// 	//Progress bar decrement
+// 	$('.progress-bar').width(trivTime + '%');
+// 	trivTime--;
+// 	//if time gets to 0
+// 	if(trivTime === -10){
+// 		userAnswer = false;
+// 		//Clears Time
+// 		clearInterval(timer);
+// 		checkAnswer();
+// 	}
+	
 // }
-// initalScreen();
+var gameOver = function(){
+	//Remove everything in gamePage section
+	$(".gamePage").empty();
+	//Remove everthing in timer section
+	//$('.gamePage').empty();
+	var scoreDiv = $("<div>");
+	scoreDiv.addClass("score");
+	scoreDiv.html("Correct: " + rightAnswer + "<br>" + "Wrong: " + wrongAnswer);
+	$(".gamePage").append(scoreDiv);
+	//Assign new div element to new Div
+	var newDiv = $("<div>");
+	//add class to new Div
+	newDiv.addClass("gameOver");
+	//add game over text
+	newDiv.text("Game Over! click Reset to play again?");
+	//Append game over text to DOM
+	$(".gamePage").append(newDiv);
+	//Create ResetButton
+	var newBtn = $("<button>");
+	//Give btn Class
+	newBtn.addClass("resetBtn");
+	//Give btn reset Text
+	newBtn.text("Reset");
+	//Append
+	$(".gamePage").append(newBtn);
+	//Reset all value
+	//trivTime = 100;
+	questionCounter = 1;
+	rightAnswer = 0;
+	wrongAnswer = 0;
+	//When reset button is clicked.......
+	$(".resetBtn").on("click",function(){
+		$(".gamePage").empty()
+		//Starts game over
+		createQuestions();
+	});
+}
 
-// function 
-// });
+start();
